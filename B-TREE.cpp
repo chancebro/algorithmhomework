33a4dd26f0b3ec1d77a9ad0b25a0a03eb61363ae
chancebro <103893729+chancebro@users.noreply.github.com>
2022-06-10 01:20:48 +0900

    Add files via upload

---
+++ B-TREE.cpp
@@ -0,0 +1,405 @@
+﻿// B-TREE.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//https://www.programiz.com/dsa/deletion-from-a-b-tree 참고하였음
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+class BTreeNode {
+    int* keys;
+    int t;
+    BTreeNode** C;
+    int n;
+    bool leaf;
+
+public:
+    BTreeNode(int _t, bool _leaf);
+
+    void traverse();
+
+    int findKey(int k);
+    void insertNonFull(int k);
+    void splitChild(int i, BTreeNode* y);
+    void deletion(int k);
+    void removeFromLeaf(int idx);
+    void removeFromNonLeaf(int idx);
+    int getPredecessor(int idx);
+    int getSuccessor(int idx);
+    void fill(int idx);
+    void borrowFromPrev(int idx);
+    void borrowFromNext(int idx);
+    void merge(int idx);
+    friend class BTree;
+};
+
+class BTree {
+    BTreeNode* root;
+    int t;//minmum degree
+
+public:
+    BTree(int _t) {
+        root = NULL;
+        t = _t;
+    }
+
+    void traverse() {
+        if (root != NULL)
+            root->traverse();
+    }
+
+    void insertion(int k);
+
+    void deletion(int k);
+};
+
+// B tree node
+BTreeNode::BTreeNode(int t1, bool leaf1) {
+    t = t1;
+    leaf = leaf1;
+
+    keys = new int[2 * t - 1];
+    C = new BTreeNode * [2 * t];
+
+    n = 0;
+}
+
+// 키 찾기
+int BTreeNode::findKey(int k) {
+    //키찾는 과정
+    int idx = 0;
+    while (idx < n && keys[idx] < k)
+        ++idx;
+    return idx;
+}
+
+// 삭제 과정
+void BTreeNode::deletion(int k) {
+    int idx = findKey(k);//일단 키를 찾고
+
+    if (idx < n && keys[idx] == k) {
+        if (leaf)
+            removeFromLeaf(idx);//leaf노드라면 리프노드에 있는 delete방식으로 delete
+        else
+            removeFromNonLeaf(idx);//아니면 non리프노드 delete 방식으로 delete
+    }
+    else {
+        if (leaf) {
+            cout << "The key " << k << " is does not exist in the tree\n";//찾는 키가 존재하지 않는 경우
+            return;
+        }
+
+        bool flag = ((idx == n) ? true : false);
+
+        if (C[idx]->n < t)
+            fill(idx);
+
+        if (flag && idx > n)
+            C[idx - 1]->deletion(k);
+        else
+            C[idx]->deletion(k);
+    }
+    return;
+}
+
+// leaf노드에서 remove
+void BTreeNode::removeFromLeaf(int idx) {
+    for (int i = idx + 1; i < n; ++i)
+        keys[i - 1] = keys[i];
+
+    n--;
+
+    return;
+}
+
+// 리프노드가 아닌 노드에서 delete
+void BTreeNode::removeFromNonLeaf(int idx) {
+    int k = keys[idx];
+
+    if (C[idx]->n >= t) {
+        int pred = getPredecessor(idx);
+        keys[idx] = pred;
+        C[idx]->deletion(pred);
+    }
+
+    else if (C[idx + 1]->n >= t) {
+        int succ = getSuccessor(idx);
+        keys[idx] = succ;
+        C[idx + 1]->deletion(succ);
+    }
+
+    else {
+        merge(idx);
+        C[idx]->deletion(k);
+    }
+    return;
+}
+//Predecessor에서 가져오는 과정
+int BTreeNode::getPredecessor(int idx) {
+    BTreeNode* cur = C[idx];
+    while (!cur->leaf)
+        cur = cur->C[cur->n];
+
+    return cur->keys[cur->n - 1];
+}
+//successor에서 가져오는 과정
+int BTreeNode::getSuccessor(int idx) {
+    BTreeNode* cur = C[idx + 1];
+    while (!cur->leaf)
+        cur = cur->C[0];
+
+    return cur->keys[0];
+}
+
+void BTreeNode::fill(int idx) {
+    if (idx != 0 && C[idx - 1]->n >= t)
+        borrowFromPrev(idx);
+
+    else if (idx != n && C[idx + 1]->n >= t)
+        borrowFromNext(idx);
+
+    else {
+        if (idx != n)
+            merge(idx);
+        else
+            merge(idx - 1);
+    }
+    return;
+}
+
+// 이전 것에서 빌려오는 과정
+void BTreeNode::borrowFromPrev(int idx) {
+    BTreeNode* child = C[idx];
+    BTreeNode* sibling = C[idx - 1];
+
+    for (int i = child->n - 1; i >= 0; --i)
+        child->keys[i + 1] = child->keys[i];
+
+    if (!child->leaf) {
+        for (int i = child->n; i >= 0; --i)
+            child->C[i + 1] = child->C[i];
+    }
+
+    child->keys[0] = keys[idx - 1];
+
+    if (!child->leaf)
+        child->C[0] = sibling->C[sibling->n];
+
+    keys[idx - 1] = sibling->keys[sibling->n - 1];
+
+    child->n += 1;
+    sibling->n -= 1;
+
+    return;
+}
+
+// 다음 것에서 빌려오는 과정
+void BTreeNode::borrowFromNext(int idx) {
+    BTreeNode* child = C[idx];
+    BTreeNode* sibling = C[idx + 1];
+
+    child->keys[(child->n)] = keys[idx];
+
+    if (!(child->leaf))
+        child->C[(child->n) + 1] = sibling->C[0];
+
+    keys[idx] = sibling->keys[0];
+
+    for (int i = 1; i < sibling->n; ++i)
+        sibling->keys[i - 1] = sibling->keys[i];
+
+    if (!sibling->leaf) {
+        for (int i = 1; i <= sibling->n; ++i)
+            sibling->C[i - 1] = sibling->C[i];
+    }
+
+    child->n += 1;
+    sibling->n -= 1;
+
+    return;
+}
+
+// 합병과정
+void BTreeNode::merge(int idx) {
+    BTreeNode* child = C[idx];
+    BTreeNode* sibling = C[idx + 1];
+
+    child->keys[t - 1] = keys[idx];
+
+    for (int i = 0; i < sibling->n; ++i)
+        child->keys[i + t] = sibling->keys[i];
+
+    if (!child->leaf) {
+        for (int i = 0; i <= sibling->n; ++i)
+            child->C[i + t] = sibling->C[i];
+    }
+
+    for (int i = idx + 1; i < n; ++i)
+        keys[i - 1] = keys[i];
+
+    for (int i = idx + 2; i <= n; ++i)
+        C[i - 1] = C[i];
+
+    child->n += sibling->n + 1;
+    n--;
+
+    delete (sibling);
+    return;
+}
+
+// 삽입과정
+void BTree::insertion(int k) {
+    if (root == NULL) {
+        root = new BTreeNode(t, true);
+        root->keys[0] = k;
+        root->n = 1;
+    }
+    else {
+        if (root->n == 2 * t - 1) {
+            BTreeNode* s = new BTreeNode(t, false);
+
+            s->C[0] = root;
+
+            s->splitChild(0, root);
+
+            int i = 0;
+            if (s->keys[0] < k)
+                i++;
+            s->C[i]->insertNonFull(k);
+
+            root = s;
+        }
+        else
+            root->insertNonFull(k);
+    }
+}
+
+// 꽉 차지 않았을 경우 insert과정
+void BTreeNode::insertNonFull(int k) {
+    int i = n - 1;
+
+    if (leaf == true) {
+        while (i >= 0 && keys[i] > k) {
+            keys[i + 1] = keys[i];
+            i--;
+        }
+
+        keys[i + 1] = k;
+        n = n + 1;
+    }
+    else {
+        while (i >= 0 && keys[i] > k)
+            i--;
+
+        if (C[i + 1]->n == 2 * t - 1) {
+            splitChild(i + 1, C[i + 1]);
+
+            if (keys[i + 1] < k)
+                i++;
+        }
+        C[i + 1]->insertNonFull(k);
+    }
+}
+
+// Split child
+void BTreeNode::splitChild(int i, BTreeNode* y) {
+    BTreeNode* z = new BTreeNode(y->t, y->leaf);
+    z->n = t - 1;
+
+    for (int j = 0; j < t - 1; j++)
+        z->keys[j] = y->keys[j + t];
+
+    if (y->leaf == false) {
+        for (int j = 0; j < t; j++)
+            z->C[j] = y->C[j + t];
+    }
+
+    y->n = t - 1;
+
+    for (int j = n; j >= i + 1; j--)
+        C[j + 1] = C[j];
+
+    C[i + 1] = z;
+
+    for (int j = n - 1; j >= i; j--)
+        keys[j + 1] = keys[j];
+
+    keys[i] = y->keys[t - 1];
+
+    n = n + 1;
+}
+
+// Traverse
+void BTreeNode::traverse() {
+    int i;
+    for (i = 0; i < n; i++) {
+        if (leaf == false)
+            C[i]->traverse();
+        cout << " " << keys[i];
+    }
+
+    if (leaf == false)
+        C[i]->traverse();
+}
+
+// 삭제 과정
+void BTree::deletion(int k) {
+    if (!root) {
+        cout << "The tree is empty\n";
+        return;
+    }
+
+    root->deletion(k);
+
+    if (root->n == 0) {
+        BTreeNode* tmp = root;
+        if (root->leaf)
+            root = NULL;
+        else
+            root = root->C[0];
+
+        delete tmp;
+    }
+    return;
+}
+
+int main() {
+    clock_t start = clock();//시간 측정시작
+    int n=7;//minimum degree 
+    BTree t(n);//b tree
+    srand((unsigned int)time(NULL));
+    int random_number[1000];//random한 생성값을 담을 배열
+    int delete_number[500];//삭제될 값을 담을 배열
+    for (int i = 0; i < 1000; i++)
+    {
+        random_number[i] = rand() % 10000;//0~9999사이의 값을 뽑는다
+        for (int t = 0; t < i; t++) {
+            if (random_number[i] == random_number[t]) {
+                i--;
+                break;
+            }
+        }
+        
+    }
+    for (int i = 0; i < 500; i++)
+        delete_number[i] = random_number[i];
+    for (int i = 0; i < 1000; i++)
+        t.insertion(random_number[i]);
+    cout << "The B-tree is: ";
+    t.traverse();
+    for (int i = 0; i < 500; i++)
+        t.deletion(delete_number[i]);
+    cout << "The B-tree is: ";
+    t.traverse();
+    clock_t end = clock();//시간 측정 끝
+    printf("Time: %lf\n", (float)(end - start) / CLOCKS_PER_SEC);
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
